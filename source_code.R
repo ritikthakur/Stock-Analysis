4d5346a9b605e4c8b287f26cce8136963075b3e5
ritikthakur <51287188+ritikthakur@users.noreply.github.com>
2021-03-24 17:30:39 +0530

    Update source_code.R

--- source_code.R
+++ source_code.R
@@ -1,9 +1,3 @@
-# This project is an attempt to forecast stock prices.
-# As stock values change with time and are affected by other indicators that may or may not substantiate its real value, we can use a time-series model that examines differences in the 
-# values in the series instead of actual values. This falls perfectly in line with the regression analysis, or more specifically, an ARIMA model.
-# The stock chosen for analysis in this project is RELIANCE.NS.
-# We perform multiple statistical checks on different forms of stock returns and program a flexible ARIMA model that can be used for any stock.
-
 # packages used in this project
 library(forecast)
 library(quantmod)
